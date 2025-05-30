d7a8e0c7ee934712f52cc5b43c7a90d2364fc9b4
artis-cmd <artis.accenture@gmail.com>
2025-05-30 16:35:40 +0900

    0530

--- git-backup.bat
+++ git-backup.bat
@@ -1,28 +1,25 @@
 ﻿@echo off
+chcp 65001 >nul
+cd /d E:\OneDrive\HTML
 
-:: 현재 폴더 기준에서 .git 있는지 확인
 if not exist .git (
-    echo [오류] .git 폴더가 없습니다. git init이 필요합니다.
+    echo [ERROR] .git folder not found. Please run git init.
     pause
     exit /b
 )
 
-:: 변경사항 스테이징
-echo 변경사항 스테이징 중...
+echo Staging changes...
 git add .
 
-:: 커밋 메시지 받기
-set /p msg=커밋 메시지를 입력하세요: 
-if "%msg%"=="" set msg=default commit
+echo Enter commit message:
+set /p msg=Commit message: 
+if "%msg%"=="" set msg=auto commit
 
-:: 커밋
-echo 커밋 중...
+echo Committing...
 git commit -m "%msg%"
 
-:: 푸시
-echo GitHub로 푸시 중...
+echo Pushing to GitHub...
 git push origin main
 
-:: 완료 메시지
-echo 백업 완료! GitHub를 확인하세요.
-pause
+echo Done! Check GitHub repository.
+pause,mn.,m,.
